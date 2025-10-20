7c9d5b6a0abc174cd1c617abbab66cb6dd789fa8
MarkRustenholtz <markrusten@gmail.com>
2025-10-21 00:41:53 +0200

    Update cache version to v25 in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cr-gendarmerie-cache-v24'; // ⚠️ change le numéro à chaque mise à jour
+const CACHE_NAME = 'cr-gendarmerie-cache-v25'; // ⚠️ change le numéro à chaque mise à jour
 const urlsToCache = [
   './',
   './index.html',
