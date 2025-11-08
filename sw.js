0deb69836e76c84f232c76922855d557dbd840aa
MarkRustenholtz <markrusten@gmail.com>
2025-11-08 05:27:18 +0100

    Update cache name to version 53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cr-gendarmerie-cache-v52'; // ⚠️ change le numéro à chaque mise à jour
+const CACHE_NAME = 'cr-gendarmerie-cache-v53'; // ⚠️ change le numéro à chaque mise à jour
 const urlsToCache = [
   './',
   './index.html',
